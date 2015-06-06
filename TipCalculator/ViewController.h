2cbce995f89c4b7b4fc2b2afa447a4cc979a8be2
eternaltung <eternaltung@gmail.com>
2015-06-06 16:57:05 +0800

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Shih-Ming Tung on 6/6/15.
+//  Copyright (c) 2015 Shih-Ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
